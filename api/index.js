3dd1c6d5990b1b17b90fed461bca5b930f262632
Mangpongza <koomponggamer@gmail.com>
2026-06-25 17:06:43 +0700

    Fix Vercel: serve static via @vercel/static, route /api/* to function

--- api/index.js
+++ api/index.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const apiRoutes = require('../routes/api');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-const publicDir = path.join(__dirname, '..');
-app.use(express.static(publicDir, { dotfiles: 'ignore' }));
-
 app.use('/api', apiRoutes);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicDir, 'index.html'));
-});
 
 module.exports = app;
